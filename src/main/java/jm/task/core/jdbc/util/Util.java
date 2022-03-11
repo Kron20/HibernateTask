ad20ae26855572ec1f06115586ad8228672da4c8
Ilya Noskov <ilyanoskov1998@yandex.ru>
2022-03-11 11:34:07 +0300

    New Fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -17,9 +18,10 @@ public class Util {
     private static final String dbURL = "jdbc:mysql://127.0.0.1:3306/testdb";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "markiz12";
+    static SessionFactory sessionFactory;
+    private static Connection connection = null;
 
     public static Connection getConnection() {
-        Connection connection = null;
         try {
             connection = DriverManager.getConnection(dbURL, USERNAME, PASSWORD);
         } catch (SQLException e) {
@@ -28,36 +30,36 @@ public class Util {
         return connection;
     }
 
-    public class HibernateSessionFactoryUtil {
-        private static SessionFactory sessionFactory;
-
-        private HibernateSessionFactoryUtil() {
-        }
-
-        public static SessionFactory getSessionFactory() {
-            if (sessionFactory == null) {
-                try {
-                    Configuration configuration = new Configuration();
-                    Properties settings = new Properties();
-                    settings.put(Environment.URL, dbURL);
-                    settings.put(Environment.USER, USERNAME);
-                    settings.put(Environment.PASS, PASSWORD);
-                    settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-                    settings.put(Environment.SHOW_SQL, "true");
-                    settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                    configuration.setProperties(settings);
-
-                    configuration.addAnnotatedClass(User.class);
-                    StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-                            .applySettings(configuration.getProperties());
-                    sessionFactory = configuration.buildSessionFactory(builder.build());
-
-                } catch (Exception e) {
-                    System.out.println("Исключение!" + e);
-                }
-            }
-            return sessionFactory;
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
-}// реализуйте настройку соеденения с БД
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.URL, dbURL);
+                settings.put(Environment.USER, USERNAME);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+
+            } catch (Exception e) {
+                System.out.println("Исключение!" + e);
+            }
+        }
+        return sessionFactory;
+    }
+}
+
 
